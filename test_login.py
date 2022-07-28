b663869b0ee3493ffdc6c63084e6a7a3fb6c0e90
andrewwood001 <90530580+andrewwood001@users.noreply.github.com>
2022-07-28 11:59:39 -0400

    Add files via upload

--- test_login.py
+++ test_login.py
@@ -8,10 +8,10 @@ import base
 import locators
 
 
-class TestLogin(test_base.TestBase):
+class TestHudlLogin(test_base.TestBase):
 
     def test_positiveLoginScenario(self):
-        """
+
         # Test steps for Positive Login Scenario
         # 1. Open Browser (Default set to Chrome)
         # 2. Navigate to "hudl.com"
@@ -19,7 +19,7 @@ class TestLogin(test_base.TestBase):
         # 4. Enter information for username and password (Information configured in "Config.ini" file)
         # 5. Click log in button and verify the site updates to main landing page
         # 6. Log out of website and close browser window
-        """
+
         base.find_element_by_xpath(self.driver, locators.LOGIN_XPATH).click()
         base.enter(self.driver, locators.PASSWORD_XPATH, self.PASSWORD)
         base.find_element_by_xpath(self.driver, locators.EMAIL_XPATH).click()
@@ -33,7 +33,7 @@ class TestLogin(test_base.TestBase):
         self.driver.quit()
 
     def test_negativeLoginScenario(self):
-        """
+
         # Test steps for Negative Login Scenario
         # 1. Open Browser (Default set to Chrome)
         # 2. Navigate to "hudl.com"
@@ -41,7 +41,7 @@ class TestLogin(test_base.TestBase):
         # 4. Enter information for username (from "Config.ini" file) and password (Automatically set to "Fake_Password")
         # 5. Click log in button, verify the site does NOT update to login page, and returns expected error message
         # 6. Log out of website and close browser window
-        """
+
         with self.assertRaises(TimeoutException):
             base.find_element_by_xpath(self.driver, locators.LOGIN_XPATH).click()
             base.enter(self.driver, locators.PASSWORD_XPATH, 'Fake_Password')
@@ -52,15 +52,14 @@ class TestLogin(test_base.TestBase):
             base.find_element_by_css(self.driver, ".styles_pageContainer_31NnIgZuiQzDKnKlPaGLsi").click()
             base.find_element_by_id(self.driver, locators.PASSWORD_ID).click()
             base.find_element_by_id(self.driver, locators.LOGIN_ID).click()
-            base.find_element_by_css(self.driver, locators.LOGOUT_CSS_SELECTOR,
-                                     condition=ec.presence_of_element_located)
+            base.find_element_by_css(self.driver, locators.LOGOUT_CSS_SELECTOR, condition=ec.presence_of_element_located)
         self.driver.quit()
 
 
 
     def test_organizationLoginScenario(self):
-    """
-    # Test steps for Organizational Login Scenario
+
+        # Test steps for Organizational Login Scenario
     # 1. Open Browser (Default set to Chrome)
     # 2. Navigate to "hudl.com"
     # 3. Click Login button and navigate to login page
@@ -71,7 +70,8 @@ class TestLogin(test_base.TestBase):
     # 8. If the email domain is not recognized as Organization (which is expected), return to the standard Login Page
     # 9. Verify the text appears "
     # 	This account can't log in with an organization yet. Please log in using your email and password."
-    # 10. Close browser window"""
+    # 10. Close browser window
+
         base.find_element_by_link_text(self.driver, "Log in").click()
         base.find_element_by_css(self.driver, ".uni-button--minimal").click()
         base.find_element_by_id(self.driver, "uniId_1", condition=ec.presence_of_element_located)
@@ -83,7 +83,7 @@ class TestLogin(test_base.TestBase):
         self.driver.close()
 
     def test_RememberMeScenario(self):
-        """
+
         # Test Steps for Remember Me Scenario
         # 1. Open Browser (Default set to Chrome)
         # 2. Navigate to "hudl.com"
@@ -95,7 +95,7 @@ class TestLogin(test_base.TestBase):
         # 8. Close browser tab (Do NOT log out)
         # 9. Open a new browser tab and navigate to hudl.com
         # 10. Verify the page loads to the "Home" page and not the "Login page"
-        """
+
         base.find_element_by_xpath(self.driver, locators.LOGIN_XPATH).click()
         base.enter(self.driver, locators.PASSWORD_XPATH, self.PASSWORD)
         base.find_element_by_xpath(self.driver, locators.EMAIL_XPATH).click()
@@ -108,7 +108,7 @@ class TestLogin(test_base.TestBase):
         base.find_element_by_css(self.driver, locators.LOGOUT_CSS_SELECTOR, condition=ec.presence_of_element_located)
 
     def test_ResetPasswordScenario(self):
-        """
+
         # Test steps for Reset Password Scenario
         # 1. Open Browser (Default set to Chrome)
         # 2. Navigate to "hudl.com"
@@ -120,7 +120,7 @@ class TestLogin(test_base.TestBase):
         # 8. Verify "Send Password Reset" button is Inactive
         # 9. Enter the Username from "config" file into the "Email" field
         # 10. Verify the "Send Password Reset" button becomes Active, but do NOT click on it
-        """
+
         base.find_element_by_xpath(self.driver, locators.LOGIN_XPATH).click()
         self.assertEqual(self.driver.current_url, 'https://www.hudl.com/login')
         base.find_element_by_xpath(self.driver, locators.NEED_HELP_XPATH).click()
